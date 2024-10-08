a2f6d809d3f18d2031e665988a7fb4c179b06354
Jeferson Reis Belchior <jeferson.reis@sysmanager.com.br>
2024-10-08 01:05:15 -0300

    update readme

--- Desafio Frontend - Interactions/cypress.config.js
+++ Desafio Frontend - Interactions/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://demoqa.com', // URL base para os testes
-    supportFile: 'cypress/support/commands.js', // Caminho para o arquivo de suporte
+   
     specPattern: 'cypress/integration/**/*.js', // Padrão para os arquivos de teste
     setupNodeEvents(on, config) {
       // Implementar listeners de eventos de Node aqui, se necessário
@@ -12,5 +12,6 @@ module.exports = defineConfig({
     viewportHeight: 720, // Altura da viewport
     screenshotOnRunFailure: true, // Captura de tela em caso de falha
     video: false, // Desativa a gravação de vídeo durante os testes
+    supportFile: false,
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Desafio-QA
+
+## Pré-requisitos
+npm install cypress --save-dev
+
+## Executar o cypress
+npx cypress open
+
+## Executando um Desafio
+
+1 - Dentro de um dos desafios abrir o gitbash
+1 - Executar npm install cypress --save-dev
+2 - Executar npx cypress open
